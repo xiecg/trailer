a8dcc8bf79f01cef6ad23a5c3c82d00e8ae281d2
xiecg <xiecg53@163.com>
2018-08-12 15:55:23 +0800

    add  puppeteer

---
+++ server/crawler/trailer-list.js
@@ -0,0 +1,58 @@
+
+const puppeteer = require('puppeteer');
+
+const url = 'https://movie.douban.com/tag/#/?sort=R&range=6,10&tags=';
+
+const sleep = time => new Promise(resolve => {
+  setTimeout(resolve, time);
+});
+
+(async () => {
+  console.log('start visit the target page.');
+
+  const browser = await puppeteer.launch({
+    args: ['-no-sandbox'],
+    dumpio: false,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: 'networkidle2',
+  });
+
+  await sleep(3000);
+
+  // 点击更多按钮
+  await page.waitForSelector('.more');
+
+  for(let i = 0; i < 1; i++) {
+    await sleep(3000);
+    await page.click('.more'); 
+  }
+
+  const reuslt = await page.evaluate(() => {
+    const { $ } = window;
+    const items = $('.list-wp a');
+    const links = [];
+
+    items.each((index, item) => {
+      const it = $(item);
+      const doubanId = it.find('div').data('id');
+      const title = it.find('.title').text();
+      const rate = Number(it.find('.rate').text());
+      const poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio');
+      links.push({
+        doubanId,
+        title,
+        rate,
+        poster
+      });
+    });
+
+    return links;
+  });
+
+  browser.close();
+
+  console.log('reuslt', reuslt);
+})();
